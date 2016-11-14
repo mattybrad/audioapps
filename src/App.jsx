8a0f246d44186bf71bb26e982b7756a96061936d
Matt Bradshaw <matthewloydbradshaw@gmail.com>
2016-11-14 23:41:10 +0000

    fixed cordova build by changing from browserhistory to hashhistory

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import createLogger from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
-import { Router, Route, Link, browserHistory } from 'react-router';
+import { Router, Route, Link, browserHistory, hashHistory } from 'react-router';
 import rootReducer from './reducers';
 import Main from './components/Main.jsx';
 import * as Pages from './pages';
@@ -22,7 +22,7 @@ class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <Router history={browserHistory}>
+        <Router history={window.cordova?hashHistory:browserHistory}>
           <Route path="" component={Main}>
             <Route path="/" component={Pages.Home} />
             <Route path="test" component={Pages.Test} />
